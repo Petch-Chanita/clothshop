388698a2ebceb9e174c84939a53b52ab7b7cac39
Chanita <chanitalomsak6800@gmail.com>
2025-07-13 23:03:12 +0700

    Swiper slide

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,88 @@
-
+"use client";
+import { useEffect, useState } from "react";
 import ProductCard from "@/components/ProductCard";
 import { products } from "@/data/products";
+import Link from "next/link";
+import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/autoplay";
+import "swiper/css/pagination";
+
+interface imageItem {
+  id: string;
+  name: string;
+  src: string;
+  alt: string;
+}
 
 export default function Home() {
+  const [randomImages, setRandomImages] = useState<imageItem[]>([]);
+
+  useEffect(() => {
+    const images: imageItem[] = [];
+
+    const candidates = products.filter((p) => p.images && p.images.length > 0);
+    const usedProductIds = new Set<string>();
+
+    while (images.length < 5 && usedProductIds.size < candidates.length) {
+      const randomIndex = Math.floor(Math.random() * candidates.length);
+      const product = candidates[randomIndex];
+
+      if (usedProductIds.has(product.id)) continue;
+      usedProductIds.add(product.id);
+
+      const image =
+        product.images[Math.floor(Math.random() * product.images.length)];
+
+      console.log("product:> ", product);
+      console.log("image:> ", image);
+
+      images.push({
+        id: product.id,
+        name: product.name,
+        src: image,
+        alt: product.name || `Product ${images.length}`,
+      });
+    }
+
+    setRandomImages(images);
+  }, [products]);
 
   return (
     <>
       <div className="bg-white">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+          {/* Swiper Slide โฆษณา */}
+          <div className="pb-10">
+            <Swiper
+              modules={[Autoplay, Pagination]}
+              autoplay={{ delay: 10000 ,pauseOnMouseEnter: true}}
+              pagination={{ clickable: true }}
+              loop={true}
+              slidesPerView={1}
+              className="w-full h-64 sm:h-80 md:h-[400px] lg:h-[480px] rounded-xl overflow-hidden"
+            >
+              {randomImages.map((image) => (
+                <SwiperSlide key={image.id}>
+                  <Link href={`/product/${image.id}`} scroll={false}>
+                    <div className="relative w-full h-full">
+                      <Image
+                        src={image.src}
+                        alt={image.alt}
+                        layout="fill"
+                        objectFit="cover"
+                        className="rounded-xl"
+                        priority
+                      />
+                    </div>
+                  </Link>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+
           <h2 className="text-2xl font-bold tracking-tight text-gray-900">
             ร้านขายเสื้อผ้า
           </h2>
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -44,25 +44,6 @@ const ProductCard: FC<ProductProps> = ({
             <p className="text-sm font-medium text-gray-900">{price}฿</p>
           </div>
         </Link>
-        {/* <div className="flex justify-center relative">
-    <button className="cursor-pointer mt-6 w-full flex items-center justify-center gap-x-2 px-4 py-2 bg-indigo-500 text-white rounded-4xl hover:bg-indigo-400 transition duration-200">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="w-5 h-5"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-        />
-      </svg>
-      เพิ่มลงตะกร้า
-    </button>
-  </div> */}
       </div>
     </>
   );
